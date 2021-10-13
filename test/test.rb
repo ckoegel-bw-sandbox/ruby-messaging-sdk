b0ad9c42a245bcde05fc38e9e6631284c48a2010
Cameron Koegel <ckoegel@bandwidth.com>
2021-10-13 16:05:31 -0400

    made tests way better

--- test/test.rb
+++ test/test.rb
@@ -35,21 +35,27 @@ class IntegrationTest < Test::Unit::TestCase
             from: BW_NUMBER,
             text: message_text
         )
-        response = $api_instance_msg.create_message(BW_ACCOUNT_ID, body)
-        $message_id = response::id
-        assert_equal(response::text, message_text, "message text does not match") # validate a message was created and its text matches
+        response = $api_instance_msg.create_message_with_http_info(BW_ACCOUNT_ID, body)
+        assert_equal(202, response[1], "incorrect response code")
+        assert_equal(29, response[0].id.length, "id not set")
+        assert_equal(BW_NUMBER, response[0].owner, "message owner not correct")
+        assert_equal(BW_NUMBER, response[0].from, "from number does not match")
+        assert_equal([USER_NUMBER], response[0].to, "to number does not match")
+        assert_equal(body.tag, response[0].tag, "tags do not match")
+        assert_equal(body.media,response[0].media,  "media does not match")
+        assert_equal(message_text, response[0].text, "message text does not match")
     end
 
     def test_get_message
         get_opts = {
-            :source_tn => "+19195892106"
+            :source_tn => BW_NUMBER,
+            :message_direction => "OUTBOUND"
         }
         response = $api_instance_msg.get_messages_with_http_info(BW_ACCOUNT_ID, get_opts)
-        #puts response[0].messages[0]
-        #puts response.messages[0].inspect
-        #assert_equal("OpenapiClient::BandwidthMessageItemtest", response.messages[0].inspect, "fail")
-        #puts response.messages[0].source_tn
-        assert_equal(response[0].messages[0].source_tn, "+19195892106", "failed to get message from BW_NUMBER")
+        assert_equal(200, response[1], "incorrect response code")
+        assert_equal(BW_ACCOUNT_ID, response[0].messages[0].account_id, "account ids do not match")
+        assert_equal("OUTBOUND", response[0].messages[0].message_direction, "message directions do not match")
+        assert_equal(BW_NUMBER, response[0].messages[0].source_tn, "failed to get message from BW_NUMBER")
     end
 
     def test_create_message_invalid_phone_number
@@ -60,13 +66,14 @@ class IntegrationTest < Test::Unit::TestCase
             text: "ruby sdk test"
         )
         begin
-            $api_instance_msg.create_message(BW_ACCOUNT_ID, body)
-            #workaround to make sure that if the error below is not raised, the build will fail
+            $api_instance_msg.create_message_with_http_info(BW_ACCOUNT_ID, body)
             assert(false, "Expected exception not raised")
         rescue OpenapiClient::ApiError => e
             resp_body =  JSON.parse(e.response_body)
-            expected_err = "\'+1invalid\' must be replaced with a valid E164 formatted telephone number"
-            assert_equal(resp_body['fieldErrors'][0]['description'], expected_err, "error description does not match expected")
+            expected_desc = "\'+1invalid\' must be replaced with a valid E164 formatted telephone number"
+            assert_equal(400, e.code, "incorrect response code")
+            assert_equal("request-validation", resp_body['type'], "response type does not match")
+            assert_equal(expected_desc, resp_body['fieldErrors'][0]['description'], "error description does not match expected")
         end
     end
 
@@ -75,27 +82,22 @@ class IntegrationTest < Test::Unit::TestCase
         media_data = '123456'
 
         #media upload
-        begin
-            up_response = $api_instance_media.upload_media(BW_ACCOUNT_ID, media_name, media_data)
-        rescue OpenapiClient::ApiError => e
-            puts "Status Code: "+ e.code
-            assert(false, "upload media failed")
-        end
+        up_response = $api_instance_media.upload_media_with_http_info(BW_ACCOUNT_ID, media_name, media_data)
+        assert_equal(204, up_response[1], "incorrect response code")
 
         #media list
-        list_media = $api_instance_media.list_media(BW_ACCOUNT_ID)
-        assert_equal(list_media[0].media_name, media_name, "could not find media on account")
+        list_media = $api_instance_media.list_media_with_http_info(BW_ACCOUNT_ID)
+        assert_equal(200, list_media[1], "incorrect response code")
+        assert_equal(media_data.length, list_media[0][0].content_length, "incorrect data length")
+        assert_equal(media_name, list_media[0][0].media_name, "could not find media on account")
 
         #media download
-        downloaded_media_file = $api_instance_media.get_media(BW_ACCOUNT_ID, media_name, debug_return_type: 'String')
-        assert_equal(downloaded_media_file, media_data, "downloaded media file not equal to upload")
+        downloaded_media_file = $api_instance_media.get_media_with_http_info(BW_ACCOUNT_ID, media_name, debug_return_type: 'String')
+        assert_equal(200, downloaded_media_file[1], "incorrect response code")
+        assert_equal(media_data, downloaded_media_file[0], "downloaded media file not equal to upload")
 
         #media delete
-        begin
-            del_response = $api_instance_media.delete_media(BW_ACCOUNT_ID, media_name)
-        rescue OpenapiClient::ApiError => e
-            puts "Status Code: "+ e.code
-            assert(false, "delete media failed")
-        end
+        del_response = $api_instance_media.delete_media_with_http_info(BW_ACCOUNT_ID, media_name)
+        assert_equal(204, del_response[1], "incorrect response code")
     end
 end

2aaecf245e68b1fa5ffa8539f5f93acc172642f9
Cameron Koegel <ckoegel@bandwidth.com>
2021-10-13 13:57:50 -0400

    added testing

--- test/test.rb
+++ test/test.rb
@@ -21,22 +21,34 @@ class IntegrationTest < Test::Unit::TestCase
         # Configure HTTP basic authorization: httpBasic
         config.username = BW_USERNAME
         config.password = BW_PASSWORD
-      end
+        #config.ssl_verify = false # remove for testing on push
+    end
     
-      $api_instance_msg = OpenapiClient::MessagesApi.new()
+    $api_instance_msg = OpenapiClient::MessagesApi.new()
+    $api_instance_media = OpenapiClient::MediaApi.new()
 
-      def test_create_message
+    def test_create_message
+        message_text = "ruby sdk test"
         body = OpenapiClient::MessageRequest.new(
             application_id: BW_MESSAGING_APPLICATION_ID,
             to: [USER_NUMBER],
             from: BW_NUMBER,
-            text: "ruby sdk test"
+            text: message_text
         )
         response = $api_instance_msg.create_message(BW_ACCOUNT_ID, body)
-        assert(response::id.length > 0, "id value not set") # validate a message was created and its id exists
-      end
+        $message_id = response::id
+        assert_equal(response::text, message_text, "message text does not match") # validate a message was created and its text matches
+    end
 
-      def test_create_message_invalid_phone_number
+    def test_get_message
+        get_opts = {
+            :source_tn => BW_NUMBER
+        }
+        response = $api_instance_msg.get_messages(BW_ACCOUNT_ID, get_opts)
+        assert_equal(response.messages[0].source_tn, BW_NUMBER, "failed to get message from BW_NUMBER")
+    end
+
+    def test_create_message_invalid_phone_number
         body = OpenapiClient::MessageRequest.new(
             application_id: BW_MESSAGING_APPLICATION_ID,
             to: ["+1invalid"],
@@ -48,7 +60,38 @@ class IntegrationTest < Test::Unit::TestCase
             #workaround to make sure that if the error below is not raised, the build will fail
             assert(false, "Expected exception not raised")
         rescue OpenapiClient::ApiError => e
-            assert(e.description.length > 0, "description value not set")
+            resp_body =  JSON.parse(e.response_body)
+            expected_err = "\'+1invalid\' must be replaced with a valid E164 formatted telephone number"
+            assert_equal(resp_body['fieldErrors'][0]['description'], expected_err, "error description does not match expected")
+        end
+    end
+
+    def test_all_media
+        media_file_name = 'ruby_media_test'
+        media_file = '123456'
+
+        #media upload
+        begin
+            up_response = $api_instance_media.upload_media(BW_ACCOUNT_ID, media_file_name, media_file)
+        rescue OpenapiClient::ApiError => e
+            puts "Status Code: "+ e.code
+            assert(false, "upload media failed")
+        end
+
+        #media list
+        list_media = $api_instance_media.list_media(BW_ACCOUNT_ID)
+        assert_equal(list_media[0].media_name, media_file_name, "could not find media on account")
+
+        #media download
+        downloaded_media_file = $api_instance_media.get_media(BW_ACCOUNT_ID, media_file_name, debug_return_type: 'String')
+        assert_equal(downloaded_media_file, media_file, "downloaded media file not equal to upload")
+
+        #media delete
+        begin
+            del_response = $api_instance_media.delete_media(BW_ACCOUNT_ID, media_file_name)
+        rescue OpenapiClient::ApiError => e
+            puts "Status Code: "+ e.code
+            assert(false, "delete media failed")
         end
     end
 end
